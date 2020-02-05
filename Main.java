a4c4977fcae08c836954ba3ad49a346cf24eff08
jonl456 <54850322+jonl456@users.noreply.github.com>
2020-02-05 14:48:19 -0800

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.lang.*;
+public class Main {
+
+
+    public static void main(String[] args) {
+        //tracks the profile logged into
+        Profile activeProfile;
+
+        //Testing
+        Profile Jons = new Profile("Jon", "Lubeck");
+	    Jons.printProfile();
+    }
+
+
+}
